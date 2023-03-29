d8a1dc679e1547d14735309135eedeaa90b6e776
Ibrahim <AbroadAbroad101@gmail.com>
2023-03-29 05:49:23 +0200

    Test

---
+++ lib/src/common_widgets/snack_bar_widget/custom_snack_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class CustomSnackBar extends GetSnackBar {
+  CustomSnackBar({
+    required String message,
+    IconData? icon,
+  }) : super(
+          message: message,
+          snackStyle: SnackStyle.FLOATING,
+          duration: const Duration(seconds: 2),
+          borderRadius: 10,
+          margin: const EdgeInsets.all(15),
+          icon: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+
+            ),
+            child: Icon(
+              icon ?? Icons.error,
+              color: Colors.red,
+            ),
+          ),
+        );
+}
